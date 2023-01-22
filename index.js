946861699dcaf48a2db566e31677bca338060bcd
Jean <etnpfj@gmail.com>
2023-01-22 15:26:12 +0900

    몽구스 모델 설정과 정보보안을 위한 config 파일 추가

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
 const mongoose = require("mongoose");
+const { User } = require("./models/User");
+const config = require("./config/key");
 mongoose.set("strictQuery", false);
 mongoose
-  .connect(
-    "mongodb+srv://jean:jean1997!@cluster0.vy1cknt.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect(config.mongoURI)
   .then(() => console.log("mongoDB connected..."))
   .catch((e) => console.log(e));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("hello");
+});
+
+app.post("/login", (req, res) => {
+  const user = new User(req.body);
+  user.save((e, userInfo) => {
+    if (e) return res.json({ sucsses: false, e });
+    return res.status(200).json({ sucsses: true });
+  });
 });
 
 app.listen(port, () => {
